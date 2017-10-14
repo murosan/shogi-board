ee31efec142e48398400fe111e715864ab471018
murosan <masashi_16n1200013@nnn.ed.jp>
2017-10-14 12:48:58 +0900

    improve configures

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+};

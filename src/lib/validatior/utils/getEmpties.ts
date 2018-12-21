7d7f850294ed509cd4f16c7bc044cdabcbcfc2fd
murosan <m.murota.dev@gmail.com>
2018-12-22 00:27:56 +0900

    :recycle: パフォーマンス優先

--- src/lib/validatior/utils/getEmpties.ts
+++ src/lib/validatior/utils/getEmpties.ts
@@ -1,17 +1,20 @@
 import { Piece, Empty } from '../../../model/shogi/Piece'
 import Point from '../../../model/shogi/Point'
 
-export type ValidateFunc = (row: number, column: number) => boolean
-
-export default function(pos: Piece[][], validate?: ValidateFunc): Point[] {
+export default function(
+  pos: Piece[][],
+  invalidRows?: number[],
+  invalidColumns?: number[]
+): Point[] {
   const points: Point[] = []
 
   for (let r = 0; r < 9; r++) {
+    if (invalidRows && invalidRows.includes(r)) continue
+
     for (let c = 0; c < 9; c++) {
-      if (pos[r][c] !== Empty) continue
-      const point: Point = { row: r, column: c }
-      if (validate && validate(r, c)) points.push(point)
-      if (!validate) points.push(point)
+      const columnIsInvalid = invalidColumns && invalidColumns.includes(r)
+      if (columnIsInvalid || pos[r][c] !== Empty) continue
+      points.push({ row: r, column: c })
     }
   }
 

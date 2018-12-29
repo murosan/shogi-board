ff59fda0f17827e09f5c83e482318e39034ce7a3
murosan <m.murota.dev@gmail.com>
2018-12-29 23:54:31 +0900

    :up: Enzyme 導入

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,9 @@
+import { mount } from 'enzyme'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  const wrapper = mount(<App />)
+  expect(wrapper.find('.App')).toHaveLength(1)
+  expect(wrapper.find('.App-BoardOnly')).toHaveLength(1)
 })

9c1ac8fc3efe079ae5dde3bfd09d0c270a4e4671
murosan <m.murota.dev@gmail.com>
2019-01-02 15:43:27 +0900

    :sparkles: 棋譜のインデックスを変更する機能

---
+++ src/lib/kif-handler/changeIndex.test.ts
@@ -0,0 +1,192 @@
+import Kif from '../../model/kif/Kif'
+import Meta from '../../model/kif/Meta'
+import Move from '../../model/kif/Move'
+import Position from '../../model/shogi/Position'
+import { Sente } from '../../model/shogi/Turn'
+import { mockMove } from '../../testutils/mockKif'
+import { changeIndex } from './changeIndex'
+
+const mockPos: Position = {
+  pos: [],
+  cap0: [],
+  cap1: [],
+  turn: Sente,
+  moveCount: 0,
+}
+const mockMeta: Meta = {
+  version: '1',
+  player: { sente: '', gote: '' },
+  handicap: '平手',
+  initPos: mockPos,
+}
+const moveMock: Move = mockMove('mock0')
+
+it('棋譜のインデックスを更新できる', async () => {
+  const kif: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [moveMock, moveMock, moveMock, moveMock, moveMock, moveMock],
+      index: 5,
+    },
+  }
+  const expected: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [moveMock, moveMock, moveMock, moveMock, moveMock, moveMock],
+      index: 0,
+    },
+  }
+  expect(changeIndex(kif, 0)).toEqual(expected)
+})
+
+it('棋譜のインデックスを更新できる2', async () => {
+  const kif: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [moveMock, moveMock, moveMock, moveMock, moveMock, moveMock],
+      index: 0,
+    },
+  }
+  const expected: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [moveMock, moveMock, moveMock, moveMock, moveMock, moveMock],
+      index: 5,
+    },
+  }
+  expect(changeIndex(kif, 5)).toEqual(expected)
+})
+
+it('分岐が入っていても更新できる', async () => {
+  const kif: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [
+        moveMock,
+        {
+          branches: [
+            { moves: [moveMock], index: 0 },
+            { moves: [moveMock, moveMock, moveMock], index: 0 },
+          ],
+          index: 1,
+        },
+      ],
+      index: 1,
+    },
+  }
+  const expected: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [
+        moveMock,
+        {
+          branches: [
+            { moves: [moveMock], index: 0 },
+            { moves: [moveMock, moveMock, moveMock], index: 2 },
+          ],
+          index: 1,
+        },
+      ],
+      index: 1,
+    },
+  }
+  expect(changeIndex(kif, 3)).toEqual(expected)
+})
+
+it('分岐が入っていても更新できる2', async () => {
+  const kif: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [
+        moveMock,
+        {
+          branches: [
+            { moves: [moveMock], index: 0 },
+            { moves: [moveMock, moveMock, moveMock], index: 0 },
+          ],
+          index: 1,
+        },
+      ],
+      index: 1,
+    },
+  }
+  const expected: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [
+        moveMock,
+        {
+          branches: [
+            { moves: [moveMock], index: 0 },
+            { moves: [moveMock, moveMock, moveMock], index: 0 },
+          ],
+          index: 1,
+        },
+      ],
+      index: 1,
+    },
+  }
+  expect(changeIndex(kif, 1)).toEqual(expected)
+})
+
+it('違う分岐を選択できる', async () => {
+  const kif: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [
+        moveMock,
+        {
+          branches: [
+            { moves: [moveMock], index: 0 },
+            {
+              moves: [
+                moveMock,
+                moveMock,
+                {
+                  branches: [
+                    { moves: [moveMock, moveMock], index: 1 },
+                    { moves: [moveMock, moveMock], index: 1 },
+                  ],
+                  index: 1,
+                },
+              ],
+              index: 0,
+            },
+          ],
+          index: 1,
+        },
+      ],
+      index: 1,
+    },
+  }
+  const expected: Kif = {
+    meta: mockMeta,
+    history: {
+      moves: [
+        moveMock,
+        {
+          branches: [
+            { moves: [moveMock], index: 0 },
+            {
+              moves: [
+                moveMock,
+                moveMock,
+                {
+                  branches: [
+                    { moves: [moveMock, moveMock], index: 0 },
+                    { moves: [moveMock, moveMock], index: 0 },
+                  ],
+                  index: 1,
+                },
+              ],
+              index: 0,
+            },
+          ],
+          index: 0,
+        },
+      ],
+      index: 1,
+    },
+  }
+  expect(changeIndex(kif, 1, 0)).toEqual(expected)
+})
---
+++ src/lib/kif-handler/changeIndex.ts
@@ -0,0 +1,49 @@
+import Branch from '../../model/kif/Branch'
+import History, { isBranch, KifComponent } from '../../model/kif/History'
+import Kif from '../../model/kif/Kif'
+
+/**
+ * 現在局面を更新した新しい棋譜を返す
+ * @param k Kif 更新したい棋譜
+ * @param x number 現在局面にしたい手数
+ * @param y number | undefined 分岐を切り替える場合のみ、分岐のインデックス
+ */
+export function changeIndex(k: Kif, x: number, y?: number): Kif {
+  return {
+    meta: k.meta,
+    history: changeHistory(k.history, x, y),
+  }
+}
+
+function changeHistory(h: History, x: number, y?: number): History {
+  const lastIndex: number = h.moves.length - 1
+  const last: KifComponent = h.moves[lastIndex]
+  const init: KifComponent[] = h.moves.slice(0, lastIndex)
+
+  if (x <= lastIndex)
+    return {
+      moves: isBranch(last) ? init.concat(changeBranch(last, 0, y)) : h.moves,
+      index: x,
+    }
+
+  return {
+    moves: init.concat(changeBranch(<Branch>last, x - lastIndex, y)),
+    index: lastIndex,
+  }
+}
+
+function changeBranch(b: Branch, x: number, y?: number): Branch {
+  const index = x === 0 && y !== undefined ? y : b.index
+
+  if (x === 0)
+    return {
+      branches: b.branches.map(h => changeHistory(h, 0)),
+      index,
+    }
+
+  const branches = b.branches.map((h: History, i: number) =>
+    i === b.index ? changeHistory(h, x, y) : h
+  )
+
+  return { branches, index }
+}

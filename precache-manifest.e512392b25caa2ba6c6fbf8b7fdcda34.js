4b83e3efe573147b059898fa3d3e34fa2dae3216
murosan <m.murota.dev@gmail.com>
2019-01-06 14:05:39 +0900

    Updates

---
+++ precache-manifest.e512392b25caa2ba6c6fbf8b7fdcda34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d625dae038781b2a4983",
+    "url": "/shogi-board/static/js/runtime~main.d625dae0.js"
+  },
+  {
+    "revision": "1a4cc32f3f54be0c38a7",
+    "url": "/shogi-board/static/js/main.1a4cc32f.chunk.js"
+  },
+  {
+    "revision": "76a321c1c958222520d8",
+    "url": "/shogi-board/static/js/1.76a321c1.chunk.js"
+  },
+  {
+    "revision": "1a4cc32f3f54be0c38a7",
+    "url": "/shogi-board/static/css/main.975be735.chunk.css"
+  },
+  {
+    "revision": "08157c61cd14ff94f0f16ee2f45af8ee",
+    "url": "/shogi-board/index.html"
+  }
+];

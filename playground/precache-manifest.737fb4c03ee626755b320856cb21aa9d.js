dca90d67545541732790b6a565d80fe7e5644aff
murosan <m.murota.dev@gmail.com>
2019-01-13 00:51:00 +0900

    [ci skip] Updates

---
+++ playground/precache-manifest.737fb4c03ee626755b320856cb21aa9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0baeb848807189f3d358f55c99758c92",
+    "url": "/shogi-board/playground/static/media/tatami.0baeb848.jpg"
+  },
+  {
+    "revision": "14e95738677d1b5e49981f5e6add24ce",
+    "url": "/shogi-board/playground/static/media/cap_stand1.14e95738.png"
+  },
+  {
+    "revision": "40b0fd2cead8bff8fb46c0ee80d90026",
+    "url": "/shogi-board/playground/static/media/cap_stand0.40b0fd2c.png"
+  },
+  {
+    "revision": "cdea8584f4c0efe88f7ee1f58be33939",
+    "url": "/shogi-board/playground/static/media/board.cdea8584.png"
+  },
+  {
+    "revision": "b940b8acc2c601d26c88",
+    "url": "/shogi-board/playground/static/js/runtime~main.b940b8ac.js"
+  },
+  {
+    "revision": "821d667442db01adcc05",
+    "url": "/shogi-board/playground/static/js/main.821d6674.chunk.js"
+  },
+  {
+    "revision": "2a72fb21616626f1fde8",
+    "url": "/shogi-board/playground/static/js/1.2a72fb21.chunk.js"
+  },
+  {
+    "revision": "821d667442db01adcc05",
+    "url": "/shogi-board/playground/static/css/main.36ab5668.chunk.css"
+  },
+  {
+    "revision": "494199d0a4584be6eb2b91fa4bbb35fb",
+    "url": "/shogi-board/playground/index.html"
+  }
+];
--- playground/service-worker.js
+++ playground/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shogi-board/precache-manifest.c9fcc01753ee41a661777c1f70b7f957.js"
+  "/shogi-board/playground/precache-manifest.737fb4c03ee626755b320856cb21aa9d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/shogi-board/index.html", {
+workbox.routing.registerNavigationRoute("/shogi-board/playground/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

f7d02f5c81a1234b79f510e6423d5a5559ecaf93
murosan <m.murota.dev@gmail.com>
2019-03-08 00:46:33 +0900

    :recycle:

--- src/model/engine/Result.ts
+++ src/model/engine/Result.ts
@@ -2,17 +2,9 @@ import EngineMove from './EngineMove'
 
 // サーバーから fetch すると取れる値
 export class Result {
-  private _values: Map<number, Info>
+  values: Map<number, Info>
   constructor() {
-    this._values = new Map()
-  }
-
-  set values(v: Map<number, Info>) {
-    this._values = v
-  }
-
-  get values() {
-    return this._values
+    this.values = new Map()
   }
 }
 

48e7a1c3375f75d0f27b72106db2b6e2a64ce285
murosan <m.murota.dev@gmail.com>
2019-03-09 00:03:39 +0900

    :up: upgrade jest

--- src/config/Config.test.ts
+++ src/config/Config.test.ts
@@ -4,11 +4,7 @@ import { config } from './Config'
 it('設定ファイルを読み込める', async () => {
   const expected: ConfigModel = {
     paintTargets: true,
-    server: {
-      protocol: 'http',
-      host: 'localhost',
-      port: 8080,
-    },
+    serverURL: 'http://localhost:8080',
   }
   expect(config).toEqual(expected)
 })

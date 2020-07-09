310ebc47b09bfd72f92863519075c6d18e6d7a71
murosan <m.murota.dev@gmail.com>
2020-07-09 19:57:03 +0900

    :bug: SVG を base64 で指定しているとビルドできなかったので修正(startはできたんだけど..)

--- src/components/shogi/Buttons.tsx
+++ src/components/shogi/Buttons.tsx
@@ -1,5 +1,14 @@
 import { observer } from 'mobx-react-lite'
 import React, { FC } from 'react'
+import ConfigurationImg from '../../img/components/buttons/configuration.svg'
+import ConnectToEngineImg from '../../img/components/buttons/connect-to-engine.svg'
+import CopyKifImg from '../../img/components/buttons/copy-kif.svg'
+import NextFiveImg from '../../img/components/buttons/next-five.svg'
+import NextOneImg from '../../img/components/buttons/next-one.svg'
+import PrevFiveImg from '../../img/components/buttons/prev-five.svg'
+import PrevOneImg from '../../img/components/buttons/prev-one.svg'
+import ReverseImg from '../../img/components/buttons/reverse.svg'
+import StopEngineImg from '../../img/components/buttons/stop-engine.svg'
 import { getAsString } from '../../lib/kif-handler/getAsString'
 import {
   MockupEngineControl,
@@ -29,32 +38,61 @@ const Buttons: FC = () => {
     await engineState.stopThinking()
   }
 
-  const connectToEngineClassName =
-    'ConnectToEngine ' +
-    (engineState.state === Thinking ? 'CTE-Img-Stop-Engine' : 'CTE-Img')
+  const bgImg = (url: string) => ({ backgroundImage: `url(${url})` })
 
   return (
     <div className="ButtonsContainer">
-      <button className="PrevOne" onClick={() => skipKif(prevOne)} />
-      <button className="NextOne" onClick={() => skipKif(nextOne)} />
-      <button className="PrevFive" onClick={() => skipKif(prevFive)} />
-      <button className="NextFive" onClick={() => skipKif(nextFive)} />
-      <button className="Reverse" onClick={() => gameState.reverse()} />
-      <button className="Copy" data-clipboard-text={getAsString(kif)} />
-      <button className={connectToEngineClassName} onClick={engineOnClick} />
+      <button
+        className="PrevOne"
+        onClick={() => skipKif(prevOne)}
+        style={bgImg(PrevOneImg)}
+      />
+      <button
+        className="NextOne"
+        onClick={() => skipKif(nextOne)}
+        style={bgImg(NextOneImg)}
+      />
+      <button
+        className="PrevFive"
+        onClick={() => skipKif(prevFive)}
+        style={bgImg(PrevFiveImg)}
+      />
+      <button
+        className="NextFive"
+        onClick={() => skipKif(nextFive)}
+        style={bgImg(NextFiveImg)}
+      />
+      <button
+        className="Reverse"
+        onClick={() => gameState.reverse()}
+        style={bgImg(ReverseImg)}
+      />
+      <button
+        className="Copy"
+        data-clipboard-text={getAsString(kif)}
+        style={bgImg(CopyKifImg)}
+      />
+      <button
+        className="ConnectToEngine"
+        onClick={engineOnClick}
+        style={
+          engineState.state === Thinking
+            ? bgImg(StopEngineImg)
+            : bgImg(ConnectToEngineImg)
+        }
+      />
       <button
         className="Configuration"
         onClick={() => displayState.setMockupState(MockupSetting)}
+        style={bgImg(ConfigurationImg)}
       />
     </div>
   )
 
   async function connectToEngine() {
     // サーバー URL が設定されてなかったら、設定画面を出す
-    if (config.serverURL === '') {
-      console.log('here')
+    if (config.serverURL === '')
       return await displayState.setMockupState(MockupServerSetting)
-    }
 
     await displayState.setMockupState(MockupEngineControl)
   }

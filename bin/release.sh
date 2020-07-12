3f4a92fc89666a22351eb68a665864900bb0d376
murosan <m.murota.dev@gmail.com>
2020-07-13 01:17:34 +0900

    :sparkles: リリース用スクリプト追加

---
+++ bin/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash -e
+
+read -ep "Version (ex. 1.0.0) > " VERSION
+cd $(dirname $0)/..
+
+SB_DIR="shogi-board-$VERSION"
+DEST="release/$SB_DIR"
+
+export PUBLIC_URL="./"
+
+yarn build
+mkdir -p $DEST
+cp -a build/* $DEST
+
+cd release
+tar cvzf $SB_DIR.tar.gz $SB_DIR
+rm -rf $SB_DIR

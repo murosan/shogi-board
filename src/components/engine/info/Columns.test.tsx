ca47dd9c1f697383e56d1ba67efb529de2b27616
murosan <m.murota.dev@gmail.com>
2022-06-16 22:38:27 +0900

    :+1: 将棋エンジンの思考結果も、2文字の棋譜表記には、間にスペースを入れて3文字にする (#85)

--- src/components/engine/info/Columns.test.tsx
+++ src/components/engine/info/Columns.test.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import shortid from 'shortid'
 import { Info } from '../../../model/engine/Info'
 import { MoveProps } from '../../../model/events/MoveProps'
-import { Fu0 } from '../../../model/shogi/Piece'
+import { newKifu } from '../../../model/kifu/Kifu'
+import { Move } from '../../../model/kifu/Move'
+import { hirate } from '../../../model/shogi/InitialPositions'
+import { Empty, Fu0, Fu1 } from '../../../model/shogi/Piece'
+import { Gote } from '../../../model/shogi/Turn'
 import { defaultStore } from '../../../store/Store'
 import { shallow } from '../../../testutils/component-helper'
 import Columns from './Columns'
@@ -45,6 +49,42 @@ it('思考結果も表示できる', async () => {
   expect(wrapper.find('.EngineInfoRowContent')).toHaveLength(2)
   expect(wrapper.text()).toContain('読み')
   expect(wrapper.text()).toContain('７六歩')
+})
+
+it('`同　歩`のように3文字以上で表記される', async () => {
+  const store = defaultStore()
+  const pos = hirate()
+  pos.pos[6][1] = Empty
+  pos.pos[3][1] = Fu0 // 24に歩を置く
+  pos.turn = Gote
+  const move: Move = {
+    index: 0,
+    str: '24歩が置かれた局面',
+    pos: pos,
+    source: { row: 6, column: 1 }, // 盤上に存在しない場所にしておく
+    dest: { row: 3, column: 1 },
+    piece: 0,
+  }
+  const kifu = newKifu()
+  kifu.history.moves[0] = move
+  store.gameState.setKifu(kifu)
+  store.engineState.result = [
+    info([
+      {
+        pos: store.gameState.currentMove.pos,
+        source: { column: 1, row: 2 },
+        dest: { column: 1, row: 3 },
+        piece: Fu1,
+      },
+    ]),
+  ]
+  const wrapper = shallow(() => <Columns />, store)
+  expect(wrapper.find('.EngineInfoColumn')).toHaveLength(1)
+  expect(wrapper.find('.EngineInfoRow')).toHaveLength(2)
+  expect(wrapper.find('.EngineInfoRowLabel')).toHaveLength(2)
+  expect(wrapper.find('.EngineInfoRowContent')).toHaveLength(2)
+  expect(wrapper.text()).toContain('読み')
+  expect(wrapper.text()).toContain('同　歩')
 })
 
 function info(moves: MoveProps[], score?: number): Info {
